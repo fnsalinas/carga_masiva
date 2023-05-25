e32d93badffc90470ce4bcebb423fea2ddf4b8e8
fabios <fabio.salinas@hdi.com.co>
2023-05-25 16:43:29 -0500

    Agregado ajuste de identación en clase

--- src/processor.py
+++ src/processor.py
@@ -19,121 +19,121 @@ class DataProcessor:
         self.end_global_date = end_global_date
         self.interval = interval
 
-def _run_process_dates(self, start_date: str, end_date: str) -> Dict[str, Any]:
-    """
-    Run a process with a start and end date and return the result
-    Args:
-        process_id (int): Process ID to run
-        start_date (str): Start date in format YYYY-MM-DD
-        end_date (str): End date in format YYYY-MM-DD
-    Returns:
-        Dict[str, Any]: Result of the process run
-    """
-    url_api=os.getenv("URL_API")
-    request = requests.get(
-        url_api,
-        params={'process_id':self.process_id, 'start_date':start_date, 'end_date':end_date},
-        auth=("admin", "admin")
-        )
-    return request.json()
+    def _run_process_dates(self, start_date: str, end_date: str) -> Dict[str, Any]:
+        """
+        Run a process with a start and end date and return the result
+        Args:
+            process_id (int): Process ID to run
+            start_date (str): Start date in format YYYY-MM-DD
+            end_date (str): End date in format YYYY-MM-DD
+        Returns:
+            Dict[str, Any]: Result of the process run
+        """
+        url_api=os.getenv("URL_API")
+        request = requests.get(
+            url_api,
+            params={'process_id':self.process_id, 'start_date':start_date, 'end_date':end_date},
+            auth=("admin", "admin")
+            )
+        return request.json()
 
 
-def _get_list_of_strdates(self, start_date: str, end_date: str) -> List[Tuple[str, str]]:
-    """
-    Create a list of tuples with start and end date for each interval between start_date and end_date
-    Args:
-        start_date (str, optional): Start date. Defaults to "2000-01-01".
-        end_date (str, optional): End date. Defaults to "2000-01-01".
-        interval (str, optional): Interval. Defaults to "month", can be "day", "month", "quarter", "semester" or "year".
-    Returns:
-        List[Tuple[str, str]]: List of tuples with start and end date for each interval between start_date and end_date
-    """
-    if self.interval == "month":
-        start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        list_of_strdates = []
-        while start_date < end_date:
-            next_date = start_date + relativedelta(months=1)
-            list_of_strdates.append((start_date.strftime("%Y-%m-%d"), next_date.strftime("%Y-%m-%d")))
-            start_date = next_date
-        return list_of_strdates
-    elif self.interval == "day":
-        start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        list_of_strdates = []
-        while start_date < end_date:
-            next_date = start_date + timedelta(days=1)
-            list_of_strdates.append((start_date.strftime("%Y-%m-%d"), next_date.strftime("%Y-%m-%d")))
-            start_date = next_date
-        return list_of_strdates
-    elif self.interval == "week":
-        start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        list_of_strdates = []
-        while start_date < end_date:
-            next_date = start_date + timedelta(days=7)
-            list_of_strdates.append((start_date.strftime("%Y-%m-%d"), next_date.strftime("%Y-%m-%d")))
-            start_date = next_date
-        return list_of_strdates
-    elif self.interval == "quarter":
-        start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        list_of_strdates = []
-        while start_date < end_date:
-            next_date = start_date + relativedelta(months=3)
-            list_of_strdates.append((start_date.strftime("%Y-%m-%d"), next_date.strftime("%Y-%m-%d")))
-            start_date = next_date
-        return list_of_strdates
-    elif self.interval == "semester":
-        start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        list_of_strdates = []
-        while start_date < end_date:
-            next_date = start_date + relativedelta(months=6)
-            list_of_strdates.append((start_date.strftime("%Y-%m-%d"), next_date.strftime("%Y-%m-%d")))
-            start_date = next_date
-        return list_of_strdates
-    elif self.interval == "year":
-        start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        list_of_strdates = []
-        while start_date < end_date:
-            next_date = start_date + relativedelta(years=1)
-            list_of_strdates.append((start_date.strftime("%Y-%m-%d"), next_date.strftime("%Y-%m-%d")))
-            start_date = next_date
-        return list_of_strdates
-    else:
-        raise ValueError("Interval not supported")
+    def _get_list_of_strdates(self, start_date: str, end_date: str) -> List[Tuple[str, str]]:
+        """
+        Create a list of tuples with start and end date for each interval between start_date and end_date
+        Args:
+            start_date (str, optional): Start date. Defaults to "2000-01-01".
+            end_date (str, optional): End date. Defaults to "2000-01-01".
+            interval (str, optional): Interval. Defaults to "month", can be "day", "month", "quarter", "semester" or "year".
+        Returns:
+            List[Tuple[str, str]]: List of tuples with start and end date for each interval between start_date and end_date
+        """
+        if self.interval == "month":
+            start_date = datetime.strptime(start_date, "%Y-%m-%d")
+            end_date = datetime.strptime(end_date, "%Y-%m-%d")
+            list_of_strdates = []
+            while start_date < end_date:
+                next_date = start_date + relativedelta(months=1)
+                list_of_strdates.append((start_date.strftime("%Y-%m-%d"), next_date.strftime("%Y-%m-%d")))
+                start_date = next_date
+            return list_of_strdates
+        elif self.interval == "day":
+            start_date = datetime.strptime(start_date, "%Y-%m-%d")
+            end_date = datetime.strptime(end_date, "%Y-%m-%d")
+            list_of_strdates = []
+            while start_date < end_date:
+                next_date = start_date + timedelta(days=1)
+                list_of_strdates.append((start_date.strftime("%Y-%m-%d"), next_date.strftime("%Y-%m-%d")))
+                start_date = next_date
+            return list_of_strdates
+        elif self.interval == "week":
+            start_date = datetime.strptime(start_date, "%Y-%m-%d")
+            end_date = datetime.strptime(end_date, "%Y-%m-%d")
+            list_of_strdates = []
+            while start_date < end_date:
+                next_date = start_date + timedelta(days=7)
+                list_of_strdates.append((start_date.strftime("%Y-%m-%d"), next_date.strftime("%Y-%m-%d")))
+                start_date = next_date
+            return list_of_strdates
+        elif self.interval == "quarter":
+            start_date = datetime.strptime(start_date, "%Y-%m-%d")
+            end_date = datetime.strptime(end_date, "%Y-%m-%d")
+            list_of_strdates = []
+            while start_date < end_date:
+                next_date = start_date + relativedelta(months=3)
+                list_of_strdates.append((start_date.strftime("%Y-%m-%d"), next_date.strftime("%Y-%m-%d")))
+                start_date = next_date
+            return list_of_strdates
+        elif self.interval == "semester":
+            start_date = datetime.strptime(start_date, "%Y-%m-%d")
+            end_date = datetime.strptime(end_date, "%Y-%m-%d")
+            list_of_strdates = []
+            while start_date < end_date:
+                next_date = start_date + relativedelta(months=6)
+                list_of_strdates.append((start_date.strftime("%Y-%m-%d"), next_date.strftime("%Y-%m-%d")))
+                start_date = next_date
+            return list_of_strdates
+        elif self.interval == "year":
+            start_date = datetime.strptime(start_date, "%Y-%m-%d")
+            end_date = datetime.strptime(end_date, "%Y-%m-%d")
+            list_of_strdates = []
+            while start_date < end_date:
+                next_date = start_date + relativedelta(years=1)
+                list_of_strdates.append((start_date.strftime("%Y-%m-%d"), next_date.strftime("%Y-%m-%d")))
+                start_date = next_date
+            return list_of_strdates
+        else:
+            raise ValueError("Interval not supported")
 
-def run_process(self):
-    """
-    Run a process with a start and end date and return the result for each interval between start_date and end_date
-    Args:
-        process_id (int): Process ID to run
-        start_global_date (str): Start date in format YYYY-MM-DD
-        end_global_date (str): End date in format YYYY-MM-DD
-        interval (str, optional): Interval. Defaults to "month", can be "day", "month", "quarter", "semester" or "year".
-    """
+    def run_full_process(self):
+        """
+        Run a process with a start and end date and return the result for each interval between start_date and end_date
+        Args:
+            process_id (int): Process ID to run
+            start_global_date (str): Start date in format YYYY-MM-DD
+            end_global_date (str): End date in format YYYY-MM-DD
+            interval (str, optional): Interval. Defaults to "month", can be "day", "month", "quarter", "semester" or "year".
+        """
 
-    dates_list = self._get_list_of_strdates(self.start_global_date, self.end_global_date)
+        dates_list = self._get_list_of_strdates(self.start_global_date, self.end_global_date)
 
-    start_time_process = datetime.now()
-    result_list = []
-    for start_date, end_date in dates_list:
-        start_time_task = datetime.now()
-        print("-"*50)
-        print(f"{datetime.now()} Start date: {start_date} - End date: {end_date}")
-        result = self.run_process_dates(self.process_id, start_date, end_date)
-        result_list.append(result)
-        print(f"{datetime.now()} Result: {result}")
-        end_time_task = datetime.now()
-        time_elapsed_task = end_time_task - start_time_task
-        print(f"{datetime.now()} Task Time elapsed: {time_elapsed_task}")
+        start_time_process = datetime.now()
+        result_list = []
+        for start_date, end_date in dates_list:
+            start_time_task = datetime.now()
+            print("-"*50)
+            print(f"{datetime.now()} Start date: {start_date} - End date: {end_date}")
+            result = self.run_process_dates(self.process_id, start_date, end_date)
+            result_list.append(result)
+            print(f"{datetime.now()} Result: {result}")
+            end_time_task = datetime.now()
+            time_elapsed_task = end_time_task - start_time_task
+            print(f"{datetime.now()} Task Time elapsed: {time_elapsed_task}")
 
-    end_time_process = datetime.now()
-    time_elapsed_process = end_time_process - start_time_process
-    print(f"{datetime.now()} Process Time elapsed: {time_elapsed_process}")
-    return result_list
+        end_time_process = datetime.now()
+        time_elapsed_process = end_time_process - start_time_process
+        print(f"{datetime.now()} Process Time elapsed: {time_elapsed_process}")
+        return result_list
 
 if __name__=='__main__':
     processor = DataProcessor(
@@ -143,4 +143,4 @@ if __name__=='__main__':
         interval="month"
         )
     
-    process_results = processor.run_process()
+    process_results = processor.run_full_process()
